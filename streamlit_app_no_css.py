ceac3e3bc64e63facdb3d1c9185c1efd6d0140e8
Gowthamdotcom-maker <kurinjiveandan@gmail.com>
2025-01-23 13:35:19 +0000

    GOOD

--- streamlit_app_no_css.py
+++ streamlit_app_no_css.py
@@ -24,7 +24,7 @@ df_reshaped = pd.read_csv('data/us-population-2010-2019-reshaped.csv')
 #######################
 # Sidebar
 with st.sidebar:
-    st.title('🏂 US Population Dashboard')
+    st.title('KPI Population Dashboard')
     
     year_list = list(df_reshaped.year.unique())[::-1]
     
